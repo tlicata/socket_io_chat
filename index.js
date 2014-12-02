d83597e2995014cb5445f79c97f3b37caff5cb6f
Tim Licata <tim@timothylicata.com>
2014-12-02 10:17:16 -0800

    root route serves a file, index.html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello, World!");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000);
