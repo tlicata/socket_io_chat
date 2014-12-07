1f7573b04c63f75fd388674d51c4ba0b6e5cf41b
Tim Licata <tim@timothylicata.com>
2014-12-06 20:32:27 -0800

    broadcast messages when user (dis)connects

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ app.get("/", function(req, res){
 
 io.on("connection", function(socket){
   console.log("a user connected");
+  socket.broadcast.emit("connect message", "a user connected");
   socket.on("chat message", function (msg) {
     console.log("message: " + msg);
     io.emit("chat message", msg);
   });
   socket.on("disconnect", function () {
     console.log("a user disconnected");
+    socket.broadcast.emit("connect message", "a user disconnected");
   });
 });
 
