055747792bd914c3f9f2080d81f788590b54b98b
Tim Licata <tim@timothylicata.com>
2014-12-14 21:25:47 -0800

    wire up Users table
    ran these commands:
    
    - createdb socket_io_chat_dev
    - sqlize db:migrate

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+var db = require("./models/index");
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  db.User.all().then(function (users) {
+    res.sendFile(__dirname + "/index.html");
+  });
 });
 
 io.on("connection", function(socket){
