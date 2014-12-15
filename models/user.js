9f876310e727d329cab0f39ad913643e08bafa3f
Tim Licata <tim@timothylicata.com>
2014-12-15 11:19:55 -0800

    use passport for auth

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var bcrypt = require("bcrypt");
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: DataTypes.STRING,
@@ -8,6 +10,14 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      },
+      generateHash: function (password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+      }
+    },
+    instanceMethods: {
+      validPassword: function (password) {
+        return bcrypt.compareSync(password, this.password);
       }
     }
   });
